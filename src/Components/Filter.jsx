2b4d46d23be3916c4341e5a4c6006666402dbca5
lalman1234 <lalman4318@gmail.com>
2025-08-28 15:06:44 +0530

    changes in filter

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -33,6 +33,17 @@ const Filter = () => {
     const [categoryOptions, setCategoryOptions] = useState([]);
     const [formatOptions, setFormatOptions] = useState([]);
     const [countryOptions, setCountryOptions] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const commitSearch = () => {
+        setFilters(prev => ({ ...prev, search: inputValue }));
+    }
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            commitSearch();
+        }
+    };
     const fetchdata = async () => {
         try {
             const response = await fetch('https://www.rytstory.com/api/data/discover-feed', {
@@ -145,7 +156,7 @@ const Filter = () => {
                         closeMenuOnSelect={false}
                         components={animatedComponents}
                         value={filters.formats}
-                        onChange={(selected) => setFilters(prev => ({ ...prev, formats: selected }))}
+                        onClick={(selected) => setFilters(prev => ({ formats: selected }))}
                         isMulti
                         options={formatOptions}
                         placeholder="Format"
@@ -183,17 +194,19 @@ const Filter = () => {
                     </div>
                 </div>
             </div>
-
             {/* Search Bar */}
             <div className="px-25 mt-5 relative">
                 <MdOutlineSearch className="absolute left-26 top-[6px] text-[#7E3AF2] text-3xl" />
                 <input
                     type="search"
                     placeholder="Search"
-                    value={filters.search}
-                    onChange={(e) => setFilters(prev => ({ ...prev, search: e.target.value }))} 
-                    className="border-1 border-gray-400 rounded-sm px-4 pl-10 py-2 w-full outline-none"
-                />
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    onClick={commitSearch}
+                    onKeyDown={handleKeyDown}
+                    className="border border-gray-400 rounded px-4 pl-10 py-2 w-full outline-none"
+                    isClearable
+                    />
             </div>
         </div>
     );
--- src/Components/Topheadlines.jsx
+++ src/Components/Topheadlines.jsx
@@ -62,6 +62,16 @@ const Topheadlines = () => {
                         }
                     });
                 }
+                else {
+                    filterConditions.push({
+                        "range": {
+                            "dateCrawled": {
+                                "gte": "now-10d/d",
+                                "lte": "now"
+                            }
+                        }
+                    });
+                }
                 if (filters.search.length > 3) {
                     filterConditions.push({
                         "multi_match": {
@@ -184,7 +194,7 @@ const Topheadlines = () => {
                     )
                 })
                 const data = await response.json();
-                console.log("data",data)
+                console.log("data", data)
                 const headlineBuckets = data?.aggregations?.["0"]?.buckets || [];
 
                 const parsedHeadlines = headlineBuckets.map(h => {
@@ -261,7 +271,7 @@ const Topheadlines = () => {
                         <div className='h-[600px] scrollbar scrollbar-thumb-[#7E3AF2] scrollbar-sky-300  overflow-y-scroll'>
                             {isLoading ? (
                                 renderCard()
-                            ) : Headlines.length > 0 ?  (
+                            ) : Headlines.length > 0 ? (
                                 Headlines.map((item, idx) => (
                                     <div key={idx} className=' max-w-full h-[120px] p-5 mt-8 rounded-[8px] flex justify-between bg-[#FFFFFF] shadow-inner shadow-gray-200'>
                                         <div className='w-[454px]  h-[80px] grid grid-cols-[20%_auto] items-center gap-6'>
@@ -279,9 +289,9 @@ const Topheadlines = () => {
                                         </div>
                                     </div>
                                 ))
-                            ):(
+                            ) : (
                                 <div className="py-10 text-center text-gray-500">No headlines found.</div>
-                            ) }
+                            )}
                         </div>
                     </div>
                     <div className='max-w-full  '>
